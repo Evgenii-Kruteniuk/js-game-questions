0141c9e88452b0a3004ba13cca8d58440f85eca7
Evgenii-Kruteniuk <muster00100@yandex.ru>
2023-03-09 11:25:44 +0300

    запрет ввода цифр в инпут

--- script.js
+++ script.js
@@ -1,34 +1,11 @@
-const buttonNode = document.querySelector(".js-batton");
-const circle1Node = document.querySelector(".js-elem1");
-const circle2Node = document.querySelector(".js-elem2");
-const circle3Node = document.querySelector(".js-elem3");
+const inputNode = document.querySelector(".js-input");
+let regex = /[0-9]/g;
 
-buttonNode.addEventListener("click", color);
+/*inputNode.oninput = function() {
+  this.value = this.value.replace(regex, "")
+}*/
 
-function color() {
-  circle3Node.classList.remove("green");
-  circle1Node.classList.add("red");
-  buttonNode.removeEventListener("click", color);
-  buttonNode.addEventListener("click", colorTwo);
-}
-function colorTwo() {
-  circle1Node.classList.remove("red");
-  circle2Node.classList.add("yellow");
-  buttonNode.removeEventListener("click", colorTwo);
-  buttonNode.addEventListener("click", colorThree);
-}
-
-function colorThree() {
-  circle2Node.classList.remove("yellow");
-  circle3Node.classList.add("green");
-  buttonNode.removeEventListener("click", colorThree);
-  buttonNode.addEventListener("click", color);
-}
-
-function asd() {
-  if (buttonNode) {
-    buttonNode.click();
-  }
-}
-
-setInterval(asd, 1000);
+inputNode.addEventListener("input", (event) => {
+  event.target.value = event.target.value.replace(regex, "");
+  console.log(event.target.value);
+});
